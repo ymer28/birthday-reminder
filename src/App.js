80db0fb3de11ce9ddd5af2846385caf4abad2f56
ymer28 <82145186+ymer28@users.noreply.github.com>
2021-07-21 17:02:37 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setPeople] = useState(data);
+  const clearAll = () => {
+    setPeople([]);
+  }
+  return (
+    <main>
+      <section className='container'>
+        <h3> {people.length} Birthdays Today</h3>
+        <List people = {people} />
+        <button onClick={clearAll}>Clear All</button>
+
+      </section>
+
+    </main>
+
+  );
+}
+
+export default App;
